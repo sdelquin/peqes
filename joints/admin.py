1e65c027ec48882163ea820c81101f9ef5844f68
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-12-30 19:10:34 +0000

    Add search fields

--- joints/admin.py
+++ joints/admin.py
@@ -5,4 +5,5 @@ from .models import Joint
 
 @admin.register(Joint)
 class JointAdmin(admin.ModelAdmin):
+    search_fields = ('target_url', 'shorten_url')
     list_display = ('target_url', 'shorten_url', 'hits', 'expires_at')
